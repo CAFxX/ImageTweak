3f2f73e6c5dda63addc006c21b03cac7d07bee1b
unknown <Carlo Alberto@.(none)>
2010-09-25 09:56:07 +0200

    ImageTweak 0.19 initial import

---
+++ defaults/preferences/imagetweak.js
@@ -0,0 +1,22 @@
+/*
+ Default preferences for ImageTweak
+ */
+
+pref("extensions.imagetweak.bgcolor", "#333333");
+pref("extensions.imagetweak.zoomexp2", "200");
+pref("extensions.imagetweak.zoomtype.full", true);
+pref("extensions.imagetweak.zoomtype.unscaled", true);
+pref("extensions.imagetweak.zoomtype.free", true);
+pref("extensions.imagetweak.zoomtype.fill", true);
+pref("extensions.imagetweak.zoomtype.default", "fit");
+pref("extensions.imagetweak.shortcut.img", true);
+pref("extensions.imagetweak.shortcut.bg", false);
+pref("extensions.imagetweak.zoomonpointer", true);
+pref("extensions.imagetweak.invertmousewheel", false);
+pref("extensions.imagetweak.invertkeyboard", false);
+pref("extensions.imagetweak.clip_movement", 3);
+pref("extensions.imagetweak.startfromtopleft", true);
+pref("extensions.imagetweak.legacyscrolling", false);
+pref("extensions.imagetweak.bordercolor", "");
+
+pref("extensions.{DB2EA31C-58F5-48b7-8D60-CB0739257904}.description", "chrome://imagetweak/locale/imagetweak.properties");
