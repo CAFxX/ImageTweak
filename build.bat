6f94fd40f92e7f58daeb8c704878d6568ac4c90a
Carlo Alberto Ferraris <github@cafxx.strayorange.com>
2011-06-20 13:34:27 +0200

    Exclude icon project file from build

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 echo Cleaning working directory...
 del ImageTweak.xpi >nul 2>nul
 echo Creating XPI...
-"%PROGRAMFILES%\7-Zip\7z" a -xr!bitcons -x!.git* -x!*.bat -mx9 -tzip ImageTweak.xpi * >nul 2>nul
+"%PROGRAMFILES%\7-Zip\7z" a -xr!bitcons -xr!newicon -x!skin\favicon.ico -x!.git* -x!build.bat -x!desktop.ini -mx9 -tzip ImageTweak.xpi * >nul 2>nul
 echo All done!
