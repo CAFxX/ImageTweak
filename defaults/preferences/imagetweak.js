785f1d4981601d5dae02c68e2393294e8ee33530
Carlo Alberto Ferraris <github@cafxx.strayorange.com>
2011-09-05 17:56:03 +0200

    Add GPLv3 boilerplate to most files

--- defaults/preferences/imagetweak.js
+++ defaults/preferences/imagetweak.js
@@ -1,3 +1,24 @@
+/*
+
+    ImageTweak
+    2006-2011 CAFxX
+    http://cafxx.strayorange.com
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 /////// Description l10n ///////
 pref("extensions.{DB2EA31C-58F5-48b7-8D60-CB0739257904}.description", "chrome://imagetweak/locale/imagetweak.properties");
 
